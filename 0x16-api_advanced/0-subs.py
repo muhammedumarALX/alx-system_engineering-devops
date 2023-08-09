f03d19ec2c845d03a3c2c4375245571dab4e6393
muhammed umar <muhammedumaralx@gmail.com>
2023-08-09 07:35:45 +0100

    task 1

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,7 +6,7 @@ import requests
 
 def number_of_subscribers(subreddit):
     """function that returns number of suscribers"""
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0'}
 
     response = requests.get(url, headers, allow_redirects=False)
