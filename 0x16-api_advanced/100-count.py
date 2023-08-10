772b3f198cb995a8aae90a42ac5b43b63f49b160
muhammed umar <muhammedumaralx@gmail.com>
2023-08-10 23:15:30 +0100

    Task 100

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -6,9 +6,9 @@
 import requests
 
 
-def count_words(subreddit, word_list, array=None, after=None):
-    if array is None:
-        array = {}
+def count_words(subreddit, word_list, word_counts, after=None):
+    if word_counts is None:
+        word_counts = {}
 
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     header = {"User-Agent": "Mozilla/5.0"}
@@ -19,22 +19,20 @@ def count_words(subreddit, word_list, array=None, after=None):
                         allow_redirects=False)
 
     if data.status_code == 200:
-        results = data.json().get("data")
-        after = results.get("after")
+        data = response.json().get("data")
+        after = data.get("after")
 
-        for entry in results.get("children"):
-            split = entry.get("data").get("title").lower().split()
-
+        for itme in data.get("children"):
+            title = item.get("data").get("titlt")
+            lowercase_title = title.lower()
             for word in word_list:
-                if word.lower() in split:
-                    times = len([t for t in split if t == word.lower()])
-                    if array.get(word) is None:
-                        array[word] = times
-                    else:
-                        array[word] += times
+                if word in word_counts:
+                    word_counts[word] += lowercase_title.count(word.lower())
+                else:
+                    word_counts[word] = lower_title.count(word.lower())
 
         if after:
-            return count_words(subreddit, word_list, array, after)
+            return count_words(subreddit, word_list, word_counts, after)
         else:
             sorted_counts = sorted(array.items(),
                                    key=lambda x: (-x[1], x[0].lower()))
